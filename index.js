77c0433e9dc5611333f7d1d7cc2b6c68b7171fa7
Grace Shih <vocalise@gmail.com>
2019-12-22 18:04:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-  console.log("The DOM has loaded");
+  let text = document.getElementById("text");
+  text.textContent = "This is really cool!"
 })
 
 console.log(
